6f50166bab131ede074fa33cf57e0fb6d3e98804
Prince Ojoi <devprinceng@gmail.com>
2025-06-21 04:01:06 +0100

    feat: Add CORS support and update package dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
-
+import cors from 'cors';
 dotenv.config();
-
+// Import routes
+import authRouter from './routes/authRoute';
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
-
-// Import routes
-import authRouter from './routes/authRoute';
-
-
 
 
 // Use routes
