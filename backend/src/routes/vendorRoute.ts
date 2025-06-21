ff6252d525db790367bf45772153dc66f615c6ff
Tegzy-cmd <omorakabenjamin3@gmail.com>
2025-06-22 00:41:00 +0100

    feat: Add image upload functionality to vendor business update route

--- backend/src/routes/vendorRoute.ts
+++ backend/src/routes/vendorRoute.ts
@@ -3,13 +3,14 @@ import { isAuthenticated } from "../middlewares/isAuthenticated";
 import {
   getVendorProfile,
   updateVendorProfile,updateVendorBusiness} from "../controllers/vendorController";
+import upload from "../middlewares/upload";
 // Importing necessary controllers for handling vendor routes
 
 const vendorRouter = express.Router();
 // Define vendor routes
 vendorRouter.get("/profile", isAuthenticated, getVendorProfile); // Get vendor profile
 vendorRouter.put("/profile", isAuthenticated, updateVendorProfile); // Update vendor profile
-vendorRouter.put("/business", isAuthenticated, updateVendorBusiness); // Update vendor business details
+vendorRouter.put("/business", isAuthenticated,upload.single("logo"), updateVendorBusiness); // Update vendor business details
 
 // Export the vendor router
 export default vendorRouter;
