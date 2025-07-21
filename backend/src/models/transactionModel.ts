2be5db2b8276215ce4b8866d9cdb4c10b844f536
Tegzy-cmd <omorakabenjamin3@gmail.com>
2025-07-21 16:42:53 +0100

    updated transaction model to

--- backend/src/models/transactionModel.ts
+++ backend/src/models/transactionModel.ts
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
+import { v4 } from "uuid";
 
 const transactionSchema = new mongoose.Schema(
     {
+        transactionId: { type: String, unique: true,}, // Unique transaction ID
+        transactionHash: { type: String},  // Blockchain transaction hash
         amount: { type: Number, required: true }, 
         vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true }, 
         productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }, // Product ID
         status: { type: String, enum: ["pending", "completed", "failed", "expired"], default: "pending" }, 
         description: { type: String, default: "" }, 
-        transactionId: { type: String, unique: true,}, // Unique transaction ID
-        transactionHash: { type: String, unique: true },
         transactionTime: { type: Date}, // Timestamp of the transaction completion
     },
     {
         timestamps: true, 
     }
 );
+transactionSchema.pre("save", function (next) {
+    if (!this.transactionHash) {
+        this.transactionHash = "temp/" + v4(); // Generate a unique transaction hash if not provided
+    }
+    next();
+});
+
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 export { Transaction };
